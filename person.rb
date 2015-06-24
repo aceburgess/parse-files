b155b3c0f04ed5f5cc573bb88194dab133028f18
Ace Burgess <burgess.ace@gmail.com>
2015-06-24 19:38:52 -0400

    Create person class

---
+++ person.rb
@@ -0,0 +1,11 @@
+class Person
+
+  def initialize args = {}
+    @first_name = args[:first_name]
+    @last_name = args[:last_name]
+    @gender = args[:gender]
+    @favorite_color = args[:favorite_color]
+    @birthdate = args[:birthdate]
+  end
+
+end
