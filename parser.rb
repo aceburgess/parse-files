44cef62d87455392f389cd1904edfd2ebe75510d
Ace Burgess <burgess.ace@gmail.com>
2015-06-24 21:12:14 -0400

    Add parser module and methods parse and find_delimiter

--- parser.rb
+++ parser.rb
@@ -1,6 +1,23 @@
 module Parser
 
-  def parse file
-    File.open
+  def self.parse file
+    opened_file = File.open file
+    lines = []
+    opened_file.each do |line|
+      delimiter = self.find_delimiter line
+      lines << line.split(delimiter).map(&:strip)
+    end
+    lines
   end
+
+  def self.find_delimiter line
+    if line.include? ','
+      return ','
+    elsif line.include? '|'
+      return '|'
+    else
+      return ' '
+    end
+  end
+
 end
