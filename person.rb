20eb8d9fa2a906690e591a0f61799fc45442a2d3
Ace Burgess <burgess.ace@gmail.com>
2015-06-24 21:31:16 -0400

    Add middle name attribute to Person to account for certain files

--- person.rb
+++ person.rb
@@ -3,6 +3,7 @@ class Person
   def initialize args = {}
     @first_name = args[:first_name]
     @last_name = args[:last_name]
+    @middle_name = args[:middle_name]
     @gender = args[:gender]
     @favorite_color = args[:favorite_color]
     @birthdate = args[:birthdate]
