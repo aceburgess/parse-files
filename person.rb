84cbc7652eb5e4f1b13160e66124388727e7a7f7
Ace Burgess <burgess.ace@gmail.com>
2015-06-24 21:58:23 -0400

    Add birthdate and gender standerdizing to Person class

--- person.rb
+++ person.rb
@@ -3,11 +3,16 @@ class Person
   def initialize args = {}
     @first_name = args[:first_name]
     @last_name = args[:last_name]
-    @gender = args[:gender]
+    @gender = convert_gender args[:gender]
     @favorite_color = args[:favorite_color]
-    @birthdate = args[:birthdate]
+    @birthdate = convert_birthdate args[:birthdate]
   end
 
-  def convert_birthday
+  def convert_birthdate birthdate
+    birthdate.gsub('-', '/')
+  end
+
+  def convert_gender gender
+    gender.downcase[0] == 'f' ? 'Female' : 'Male'
   end
 end
