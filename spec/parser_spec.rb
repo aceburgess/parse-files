2e6425f45f805f0bd284414b6e341bcc6a1382e2
Ace Burgess <burgess.ace@gmail.com>
2015-06-28 22:47:48 -0400

    Add spec file for parser.rb testing the Parser module

---
+++ spec/parser_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../parser'
+
+describe Parser do
+
+  let(:comma_delimited_file) {'ruby-code-test-files/comma_delimited.txt'}
+  let(:pipe_delimited_file) {'ruby-code-test-files/pipe_delimited.txt'}
+  let(:space_delimited_file) {'ruby-code-test-files/space_delimited.txt'}
+
+  it 'Parser.parse should take a file open it and output and array' do
+    expect(Parser.parse(comma_delimited_file).class).to eq(Array)
+    expect(Parser.parse(comma_delimited_file).length).to be > 3
+  end
+
+  it 'Parser.parse should return an Array with the first value being the delimter' do
+    expect(Parser.parse(comma_delimited_file).first).to eq(',')
+    expect(Parser.parse(pipe_delimited_file).first).to eq('|')
+    expect(Parser.parse(space_delimited_file).first).to eq(' ')
+  end
+
+  it 'Parser.create_people_objects should return an Array of Person objects' do
+  end
+
+end
